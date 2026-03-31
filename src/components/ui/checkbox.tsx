8e5047cbcc92906b0bc0d33ebac13b1da07e9167
narahub123 <parknara0213@gmail.com>
2026-03-31 21:36:03 +0900

    refactor: #88 - shadcn checkbox 컴포넌트 수정

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,13 +1,12 @@
-// src/components/ui/checkbox.tsx
 "use client";
 
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "lucide-react";
+import { Checkbox as CheckboxPrimitive } from "radix-ui";
 import * as React from "react";
 
 import { cn } from "@/lib/utils/cn";
 
-export const Checkbox = React.forwardRef<
+const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
 >(({ className, ...props }, ref) => {
@@ -16,19 +15,18 @@ export const Checkbox = React.forwardRef<
       ref={ref}
       data-slot="checkbox"
       className={cn(
-        "peer h-4 w-4 shrink-0 rounded-sm border border-input shadow-sm",
-        "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring",
-        "disabled:cursor-not-allowed disabled:opacity-50",
-        "data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+        "peer size-4 shrink-0 rounded-[4px] border border-input shadow-sm transition-shadow outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
         className,
       )}
       {...props}
     >
       <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current">
-        <Check className="h-3.5 w-3.5" />
+        <Check className="size-3.5" />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   );
 });
 
 Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
