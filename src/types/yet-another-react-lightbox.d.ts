73cb33a5995d1fd93f0f061976c327919ed722c0
narahub123 <parknara0213@gmail.com>
2026-07-24 15:51:08 +0900

    feat: #255 - 관리자 이미지 Lightbox 공통 컴포넌트 추가

---
+++ src/types/yet-another-react-lightbox.d.ts
@@ -0,0 +1,7 @@
+/**
+ * yet-another-react-lightbox에서 제공하는 전역 스타일시트 모듈 선언입니다.
+ *
+ * TypeScript의 noUncheckedSideEffectImports 옵션이 활성화된 환경에서
+ * 패키지 CSS의 side-effect import를 허용합니다.
+ */
+declare module "yet-another-react-lightbox/styles.css";
