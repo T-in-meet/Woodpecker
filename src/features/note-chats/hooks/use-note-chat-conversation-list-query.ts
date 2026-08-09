5879977d501ab6d359e7d46b46c23e2e90e56eee
narahub123 <parknara0213@gmail.com>
2026-08-09 09:16:07 +0900

    feat: #296 - 노트 챗봇 대화 상태 관리 훅 추가

--- src/features/note-chats/hooks/use-note-chat-conversation-list-query.ts
+++ src/features/note-chats/hooks/use-note-chat-conversation-list-query.ts
@@ -5,12 +5,30 @@ import { useQuery } from "@tanstack/react-query";
 import { noteChatQueryKeys } from "../constants/query-keys";
 import { getNoteChatConversationList } from "../queries";
 
+type UseNoteChatConversationListQueryParams = {
+  page?: number;
+  search?: string;
+};
+
 /**
  * 노트 챗봇 대화 목록 Query입니다.
+ *
+ * 검색 및 페이지 조건을 전달하지 않으면
+ * 첫 번째 페이지의 전체 대화 목록을 조회합니다.
  */
-export function useNoteChatConversationListQuery() {
+export function useNoteChatConversationListQuery({
+  page = 1,
+  search = "",
+}: UseNoteChatConversationListQueryParams = {}) {
   return useQuery({
-    queryKey: noteChatQueryKeys.conversationList(),
-    queryFn: getNoteChatConversationList,
+    queryKey: noteChatQueryKeys.conversationList({
+      page,
+      search,
+    }),
+    queryFn: () =>
+      getNoteChatConversationList({
+        page,
+        search,
+      }),
   });
 }
